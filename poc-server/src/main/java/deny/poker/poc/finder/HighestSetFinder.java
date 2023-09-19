fd4e0a13d88c16c2f71c1a27a830e59289542c82
damianm <damian.marek@incata.it>
2023-09-20 00:22:15 +0200

    implement HighestSetFinder

--- poc-server/src/main/java/deny/poker/poc/finder/HighestSetFinder.java
+++ poc-server/src/main/java/deny/poker/poc/finder/HighestSetFinder.java
@@ -1,15 +1,45 @@
 package deny.poker.poc.finder;
 
 import deny.poker.poc.Card;
+import deny.poker.poc.SetRank;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class HighestSetFinder implements SetFinder {
+    private final EnumMap<SetRank, SetFinder> setFinderMap = new EnumMap<>(SetRank.class);
+
+    @Autowired
+    HighestSetFinder(HighCardFinder highCardFinder,
+                     HighestPairFinder highestPairFinder,
+                     HighestTwoPairsFinder highestTwoPairsFinder,
+                     HighestStraightFinder highestStraightFinder,
+                     HighestTrioFinder highestTrioFinder,
+                     HighestFullHouseFinder highestFullHouseFinder,
+                     HighestFourOfKindFinder highestFourOfKindFinder,
+                     HighestColorFinder highestColorFinder,
+                     HighestPokerFinder highestPokerFinder) {
+        setFinderMap.put(SetRank.HIGH_CARD, highCardFinder);
+        setFinderMap.put(SetRank.PAIR, highestPairFinder);
+        setFinderMap.put(SetRank.TWO_PAIRS, highestTwoPairsFinder);
+        setFinderMap.put(SetRank.STRAIGHT, highestStraightFinder);
+        setFinderMap.put(SetRank.THREE_OF_A_KIND, highestTrioFinder);
+        setFinderMap.put(SetRank.FULL_HOUSE, highestFullHouseFinder);
+        setFinderMap.put(SetRank.FOUR_OF_A_KIND, highestFourOfKindFinder);
+        setFinderMap.put(SetRank.COLOR, highestColorFinder);
+        setFinderMap.put(SetRank.POKER, highestPokerFinder);
+    }
+
     @Override
     public Optional<List<Card>> find(List<Card> cards) {
-        return Optional.empty();
+        return Arrays.stream(SetRank.values())
+                .sorted(Comparator.comparingInt(SetRank::getRank))
+                .sorted(Comparator.reverseOrder())
+                .map(setRank -> setFinderMap.get(setRank).find(cards))
+                .filter(Optional::isPresent)
+                .findFirst()
+                .orElse(Optional.empty());
     }
 }
