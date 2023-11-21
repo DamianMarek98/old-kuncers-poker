e1e0d48fb158678f74e2b14f57335f11cdfc23a5
damianm <damian.marek@incata.it>
2023-11-21 23:32:45 +0100

    add high card checker tests

--- poc-server/src/test/java/deny/poker/poc/checker/HighCardCheckerTest.java
+++ poc-server/src/test/java/deny/poker/poc/checker/HighCardCheckerTest.java
@@ -1,12 +1,34 @@
 package deny.poker.poc.checker;
 
+import deny.poker.poc.Card;
+import deny.poker.poc.Color;
+import deny.poker.poc.Figure;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class HighCardCheckerTest {
     @Autowired
     HighCardChecker highCardChecker;
 
-    //todo
+    @Test
+    void shouldReturnFalseWhenNoCards() {
+        assertFalse(highCardChecker.contains(List.of(), Figure.ACE, null));
+    }
+
+    @Test
+    void shouldReturnFalseWhenNoCardOfSpecifiedFigure() {
+        assertFalse(highCardChecker.contains(List.of(new Card(Color.BLACK_CLUB, Figure.JACK), new Card(Color.BLACK_CLUB, Figure.QUEEN)), Figure.ACE, null));
+    }
+
+    @Test
+    void shouldReturnTrueWhenCardOfGivenFigureExists() {
+        assertTrue(highCardChecker.contains(List.of(new Card(Color.BLACK_CLUB, Figure.ACE), new Card(Color.BLACK_CLUB, Figure.QUEEN)), Figure.ACE, null));
+    }
 }
